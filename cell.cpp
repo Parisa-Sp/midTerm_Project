d4d1c7215917c0c87578e8daafa32774581f834a
Parisa-Sp <109459244+Parisa-Sp@users.noreply.github.com>
2023-03-27 15:24:12 +0430

    complement method for RNA added.

--- cell.cpp
+++ cell.cpp
@@ -27,5 +27,35 @@ public:
         return DNA2;
     }
 
+    string get_DNA_complement(string rna) {
+        string complement = "";
+        for (int i = 0; i < rna.length(); i++) {
+            if (rna[i] == 'A') {
+                complement += 'T';
+            }
+            else if (rna[i] == 'T') {
+                complement += 'A';
+            }
+            else if (rna[i] == 'C') {
+                complement += 'G';
+            }
+            else if (rna[i] == 'G') {
+                complement += 'C';
+            }
+        }
+        return complement;
+    }
 
+    void create_DNA2() {
+        DNA2 = get_DNA_complement(DNA1);
+    }
 };
+
+int main() {
+    DNARNA myDNA("ATCGATCG", "CGCGCG");
+    myDNA.create_DNA2();
+    cout << "RNA: " << myDNA.get_RNA() << endl;
+    cout << "DNA1: " << myDNA.get_DNA1() << endl;
+    cout << "DNA2: " << myDNA.get_DNA2() << endl;
+    return 0;
+}
